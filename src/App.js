3198e08b716b9e35efe3df57fa85689385429a87
Shemona Singh <singhshemona@gmail.com>
2023-06-06 23:23:04 +0200

    switched to nivo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+// import { useState } from 'react';
 import './App.css';
-import sketch from './sketch';
-import { ReactP5Wrapper } from 'react-p5-wrapper';
+import { Network } from '@nivo/network'
+import { data } from './data.js'
 
 function App() {
+  // const [nodes, setNodes] = useState([])
+  // const addNode = setNodes(nodes => [...nodes, createNewNode()])
+
+  // const createNewNode = () => {
+  //   return {
+  //     id: nodes.length + 1,
+  //     text: 'some sample text',
+  //     connections: [],
+  //   }
+  // }
+
+  const CustomNodeComponent = ({ node }) => (
+    <g transform={`translate(${node.x - 12},${node.y - 18})`}>
+        <circle cx="12" cy="8" r="5" fill={node.color} stroke="#ffffff" />
+        <path d="M3,21 h18 C 21,12 3,12 3,21" fill={node.color} stroke="#ffffff" />
+        <p>{node.id}</p>
+    </g>
+  )
+
   return (
     <div className="App">
       <header>
@@ -14,9 +34,35 @@ function App() {
         </menu>
         <h1>Section 1: The Big Picture</h1>
       </header>
-      <p>hey!</p>
+      {/* <div height="1000" width="1000"> */}
+        <Network
+          height="500"
+          width="500"
+          data={data}
+          nodeComponent={CustomNodeComponent}
+          linkDistance={e=>e.distance}
+          centeringStrength={0.3}
+          repulsivity={6}
+          nodeSize={n=>n.size}
+          activeNodeSize={n=>1.5*n.size}
+          nodeColor={e=>e.color}
+          nodeBorderWidth={1}
+          nodeBorderColor={{
+              from: 'color',
+              modifiers: [
+                  [
+                      'darker',
+                      0.8
+                  ]
+              ]
+          }}
+          linkThickness={n=>2+2*n.target.data.height}
+          linkBlendMode="multiply"
+          motionConfig="wobbly"
+      />
 
-      <ReactP5Wrapper sketch={sketch} />
+      {/* </div> */}
+      
       
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const data = {
+  "nodes": [
+    {
+      "id": "Node 1",
+      "height": 1,
+      "size": 24,
+      "color": "rgb(97, 205, 187)"
+    },
+    {
+      "id": "Node 2",
+      "height": 1,
+      "size": 24,
+      "color": "rgb(97, 205, 187)"
+    },
+    {
+      "id": "Node 3",
+      "height": 1,
+      "size": 24,
+      "color": "rgb(97, 205, 187)"
+    },
+  ],
+  "links": [
+    {
+      "source": "Node 1",
+      "target": "Node 2",
+      "distance": 80
+    },
+    {
+      "source": "Node 1",
+      "target": "Node 3",
+      "distance": 50
+    },
+    {
+      "source": "Node 3",
+      "target": "Node 1",
+      "distance": 60
+    },
+  ]
+}
