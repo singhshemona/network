e27f70522972c59ab469f67bc22337d9d5831c57
Shemona Singh <singhshemona@gmail.com>
2023-07-19 23:25:13 +0200

    make dropdown go away when you click outside of it

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { DropdownContainer, DropdownContents, Trigger } from './DropdownStyles';
 
 type DropdownProps = {
@@ -8,9 +8,23 @@ type DropdownProps = {
 
 export const Dropdown = ({ trigger, contents }: DropdownProps) => {
   const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setIsOpen(false)
+      }
+    };
+    document.addEventListener('click', handleClickOutside, true);
+    
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  }, [isOpen]);
 
   return (
-    <DropdownContainer>
+    <DropdownContainer ref={ref}>
       <Trigger isOpen={isOpen} onClick={() => setIsOpen(prevState => !prevState)}>{trigger} {isOpen ? '-' : '+'} </Trigger>
       {isOpen &&
         <DropdownContents>
